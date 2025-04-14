dd39ea43ab03b36549b10c4b87b6af23e0393c7f
NatalieTanML <nat130613@gmail.com>
2025-04-14 22:07:51 +0800

    feat: add ippodo tracker

--- index.ts
+++ index.ts
@@ -9,14 +9,30 @@ config();
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID!;
-const CATEGORY_URLS = [
-  "https://www.sazentea.com/en/products/c85-yamamasa-koyamaen-matcha",
-  "https://www.sazentea.com/en/products/c24-marukyu-koyamaen-matcha",
-  "https://www.sazentea.com/en/products/c114-kanbayashi-shunsho-matcha",
-  "https://www.sazentea.com/en/products/c25-hekisuien-matcha",
-  "https://www.sazentea.com/en/products/c41-horii-shichimeien-matcha",
-  "https://www.sazentea.com/en/products/c26-hokoen-matcha",
-];
+
+const WEBSITES = {
+  SAZEN: {
+    name: "Sazen Tea",
+    shouldRefetch: false,
+    inventoryFile: "sazen-matcha.json",
+    categoryUrls: [
+      "https://www.sazentea.com/en/products/c85-yamamasa-koyamaen-matcha",
+      "https://www.sazentea.com/en/products/c24-marukyu-koyamaen-matcha",
+      "https://www.sazentea.com/en/products/c114-kanbayashi-shunsho-matcha",
+      "https://www.sazentea.com/en/products/c25-hekisuien-matcha",
+      "https://www.sazentea.com/en/products/c41-horii-shichimeien-matcha",
+      "https://www.sazentea.com/en/products/c26-hokoen-matcha",
+    ],
+  },
+  IPPODO: {
+    name: "Ippodo Tea",
+    shouldRefetch: false,
+    inventoryFile: "ippodo-matcha.json",
+    categoryUrls: ["https://global.ippodo-tea.co.jp/collections/matcha"],
+  },
+};
+
+type WebsiteKey = keyof typeof WEBSITES;
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
 
@@ -27,112 +43,156 @@ const HEADERS = {
 
 // To prevent spamming too much if the in-stock doesn't change between checks
 const STOCK_FILE = path.join(__dirname, "previous_stock.json");
+// website, urls
+type WebsiteStockMap = Record<WebsiteKey, string[]>;
 
-function readPreviousStock(): Set<string> {
-  if (fs.existsSync(STOCK_FILE)) {
-    try {
-      const data = JSON.parse(fs.readFileSync(STOCK_FILE, "utf8"));
-      return new Set(data);
-    } catch (error) {
-      console.error("Error parsing previous stock file:", error);
-    }
-  }
-  return new Set();
+function readPreviousStock(): WebsiteStockMap {
+  return fs.existsSync(STOCK_FILE) ? JSON.parse(fs.readFileSync(STOCK_FILE, "utf8")) : { SAZEN: [], IPPODO: [] };
 }
 
-function savePreviousStock(stock: Set<string>) {
-  fs.writeFileSync(STOCK_FILE, JSON.stringify(Array.from(stock)), "utf8");
+function savePreviousStock(stockMap: WebsiteStockMap) {
+  fs.writeFileSync(STOCK_FILE, JSON.stringify(stockMap, null, 2), "utf8");
 }
 
-function readProductsFromFile(): { manufacturer: string; name: string; url: string }[] | null {
-  const filePath = path.join(__dirname, "matcha.json");
+function readProductsFromFile(
+  inventoryFile: string
+): { website: WebsiteKey; manufacturer: string; name: string; url: string }[] {
+  const filePath = path.join(__dirname, inventoryFile);
   if (fs.existsSync(filePath)) {
     const data = fs.readFileSync(filePath, "utf8");
-    try {
-      return JSON.parse(data);
-    } catch (error) {
-      console.error("Error parsing JSON file:", error);
-      return null;
-    }
+    return JSON.parse(data);
   } else {
-    console.log("matcha.json file not found.");
-    return null;
+    console.log(`${inventoryFile} file not found.`);
+    return [];
   }
 }
 
-async function fetchProductLinks(): Promise<{ manufacturer: string; name: string; url: string }[]> {
-  const products = new Map<string, { manufacturer: string; name: string }>();
+async function updateProductLinks() {
+  // Sazen
+  if (WEBSITES.SAZEN.shouldRefetch) {
+    const products = new Map<string, { manufacturer: string; name: string }>();
 
-  for (const categoryUrl of CATEGORY_URLS) {
+    for (const categoryUrl of WEBSITES.SAZEN.categoryUrls) {
+      try {
+        const response = await axios.get(categoryUrl, { headers: HEADERS });
+        const $ = cheerio.load(response.data);
+
+        // 0. Get manufacturer name (h1 tag)
+        const manufacturer = $("div#content h1").first().text().trim();
+
+        // 1. Select links inside <div class="product-name">
+        $('div.product-name a[href^="/en/products/"]').each((_, element) => {
+          const name = $(element).text().trim();
+          const url = "https://www.sazentea.com" + $(element).attr("href");
+          products.set(url, { manufacturer, name });
+        });
+
+        // 2. Select the second column <td> in each row <tr>
+        $("tr").each((_, row) => {
+          const secondColumn = $(row).find('td:nth-child(2) a[href^="/en/products/"]');
+          if (secondColumn.length > 0) {
+            const name = secondColumn.text().trim();
+            const url = "https://www.sazentea.com" + secondColumn.attr("href");
+            products.set(url, { manufacturer, name });
+          }
+        });
+
+        // Update products link file
+        const mapped = Array.from(products.entries()).map(([url, { manufacturer, name }]) => ({
+          website: "SAZEN",
+          manufacturer,
+          name,
+          url,
+        }));
+        const jsonData = JSON.stringify(mapped, null, 2);
+        fs.writeFileSync(WEBSITES.SAZEN.inventoryFile, jsonData, "utf8");
+      } catch (error) {
+        console.error("Error fetching category page: ", categoryUrl, error);
+      }
+    }
+  }
+
+  // Ippodo
+  if (WEBSITES.IPPODO.shouldRefetch) {
+    // url, product name
+    const products = new Map<string, string>();
     try {
-      const response = await axios.get(categoryUrl, { headers: HEADERS });
+      const response = await axios.get(WEBSITES.IPPODO.categoryUrls[0], { headers: HEADERS });
       const $ = cheerio.load(response.data);
 
-      // 0. Get manufacturer name (h1 tag)
-      const manufacturer = $("div#content h1").first().text().trim();
-
-      // 1. Select links inside <div class="product-name">
-      $('div.product-name a[href^="/en/products/"]').each((_, element) => {
+      // 1. Select links inside <a class="a-link-product--type01">
+      $("a.a-link-product--type01").each((_, element) => {
         const name = $(element).text().trim();
-        const url = "https://www.sazentea.com" + $(element).attr("href");
-        products.set(url, { manufacturer, name });
+        const url = "https://global.ippodo-tea.co.jp" + $(element).attr("href");
+        products.set(url, name);
       });
 
-      // 2. Select the second column <td> in each row <tr>
-      $("tr").each((_, row) => {
-        const secondColumn = $(row).find('td:nth-child(2) a[href^="/en/products/"]');
-        if (secondColumn.length > 0) {
-          const name = secondColumn.text().trim();
-          const url = "https://www.sazentea.com" + secondColumn.attr("href");
-          products.set(url, { manufacturer, name });
-        }
-      });
+      // Update products link file
+      const manufacturer = WEBSITES.IPPODO.name;
+      const mapped = Array.from(products.entries()).map(([url, name]) => ({
+        website: "IPPODO",
+        manufacturer,
+        name,
+        url,
+      }));
+      const jsonData = JSON.stringify(mapped, null, 2);
+      fs.writeFileSync(WEBSITES.IPPODO.inventoryFile, jsonData, "utf8");
     } catch (error) {
-      console.error("Error fetching category page:", categoryUrl, error);
+      console.error("Error fetching Ippodo page: ", error);
     }
   }
-
-  return Array.from(products.entries()).map(([url, { manufacturer, name }]) => ({ manufacturer, name, url }));
 }
 
-async function checkStockStatus(product: { manufacturer: string; name: string; url: string }): Promise<boolean> {
-  try {
-    const response = await axios.get(product.url, { headers: HEADERS });
-    const $ = cheerio.load(response.data);
+async function checkStockStatus(product: {
+  website: WebsiteKey;
+  manufacturer: string;
+  name: string;
+  url: string;
+}): Promise<boolean> {
+  if (product.website === "SAZEN") {
+    try {
+      const response = await axios.get(product.url, { headers: HEADERS });
+      const $ = cheerio.load(response.data);
 
-    const outOfStockText = $("p strong.red").text().trim();
-    const inStockForm = $("form#basket-add");
+      const outOfStockText = $("p strong.red").text().trim();
+      const inStockForm = $("form#basket-add");
 
-    return !outOfStockText.includes("This product is unavailable") && inStockForm.length > 0;
-  } catch (error) {
-    console.error(`Error fetching product page (${product.url}):`, error);
-    return false;
+      return !outOfStockText.includes("This product is unavailable") && inStockForm.length > 0;
+    } catch (error) {
+      console.error(`Error fetching product page (${product.url}):`, error);
+      return false;
+    }
+  } else if (product.website === "IPPODO") {
+    try {
+      const response = await axios.get(product.url, { headers: HEADERS });
+      const $ = cheerio.load(response.data);
+
+      // Look for any button inside .product-form__buttons without style="display: none"
+      const visibleAddToCartButton = $(".product-form__buttons button").filter((_, el) => {
+        const style = $(el).attr("style") || "";
+        return !style.includes("display: none");
+      });
+
+      return visibleAddToCartButton.length > 0;
+    } catch (error) {
+      console.error(`Error fetching product page (${product.url}):`, error);
+      return false;
+    }
   }
+  return false;
 }
 
 async function sendGroupedTelegramMessage(
+  websiteKey: WebsiteKey,
   productsInStock: { manufacturer: string; name: string; url: string }[],
   timestamp: string
 ) {
   if (productsInStock.length > 0) {
-    const currentProductUrls = new Set(productsInStock.map((product) => product.url));
-    const previousInStockProducts = readPreviousStock();
-
-    if (
-      previousInStockProducts.size === currentProductUrls.size &&
-      [...previousInStockProducts].every((url) => currentProductUrls.has(url))
-    ) {
-      console.log("No change in stock. Skipping message.");
-      return;
-    }
-
-    savePreviousStock(currentProductUrls);
-
     const productList = productsInStock
       .map((product, index) => `${index + 1}. <a href="${product.url}">${product.manufacturer} - ${product.name}</a>`)
       .join("\n");
 
-    const message = `<b>${timestamp}</b>\n\nThe following matcha is back in stock:\n${productList}`;
+    const message = `<b>${timestamp}</b>\n\nThe following matcha is back in stock on <b><u>${websiteKey}</u></b>:\n${productList}`;
 
     console.log(message);
     await bot.telegram.sendMessage(TELEGRAM_CHAT_ID, message, {
@@ -148,48 +208,58 @@ async function main() {
     timeStyle: "long",
     timeZone: "Asia/Singapore",
   });
-
   const jstHour = now.getUTCHours() + 9;
-
   console.log("main called", timestamp, jstHour);
 
-    const products = await readProductsFromFile();
-    const productsInStock: {
-      manufacturer: string;
-      name: string;
-      url: string;
-    }[] = [];
+  const previousStockMap = readPreviousStock();
+  const currentStockMap: WebsiteStockMap = { SAZEN: [], IPPODO: [] };
+  const inStockProducts: {
+    [K in WebsiteKey]: { manufacturer: string; name: string; url: string }[];
+  } = { SAZEN: [], IPPODO: [] };
 
-    if (!products) {
-      console.log("No products from file.");
-      return;
-    }
+  for (const websiteKey of Object.keys(WEBSITES) as WebsiteKey[]) {
+    const website = WEBSITES[websiteKey];
+    const products = readProductsFromFile(website.inventoryFile);
+    const productsInStock = [];
 
     for (const product of products) {
       const isInStock = await checkStockStatus(product);
       if (isInStock) {
         productsInStock.push(product);
+        currentStockMap[websiteKey].push(product.url);
       }
     }
 
-    if (productsInStock.length > 0) {
-      await sendGroupedTelegramMessage(productsInStock, timestamp);
+    // Check for changes
+    const hasChanged =
+      previousStockMap[websiteKey]?.length !== currentStockMap[websiteKey].length ||
+      !previousStockMap[websiteKey]?.every((url) => currentStockMap[websiteKey].includes(url));
+
+    if (hasChanged && productsInStock.length > 0) {
+      inStockProducts[websiteKey] = productsInStock;
     } else {
-      console.log("No products in stock.");
-      return;
+      console.log(`No stock change for ${website.name}. Skipping message.`);
     }
+  }
+
+  // Save updated stock
+  savePreviousStock(currentStockMap);
+
+  // Send messages only for changed websites
+  for (const websiteKey of Object.keys(inStockProducts) as WebsiteKey[]) {
+    if (inStockProducts[websiteKey].length > 0) {
+      await sendGroupedTelegramMessage(websiteKey, inStockProducts[websiteKey], timestamp);
+    }
+  }
 }
 
 // NOTE: THIS WILL REMOVE/REPLACE PREVIOUSLY SAVED ITEMS. DO NOT RUN UNLESS NEEDED
-// async function saveProductsToFile() {
-//   const products = await fetchProductLinks();
-//   const jsonData = JSON.stringify(products, null, 2);
-//   fs.writeFileSync("matcha.json", jsonData, "utf8");
-//   console.log("Products data has been saved to matcha.json");
-// }
-
-// Uncomment this to update matcha list
-// saveProductsToFile();
+// (async () => {
+//   console.log("Updating inventory...");
+//   await updateProductLinks();
+//   console.log("Inventory updated.");
+//   process.exit(0); // Ensure the script exits after running
+// })();
 
 (async () => {
   console.log("Running bot script...");
